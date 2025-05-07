8d4600e2de500ade847e910f363d86dc171b9219
Umeshgardas23 <ugtech23@gmail.com>
2025-05-07 19:16:55 +0530

    welcome testing

--- server.js
+++ server.js
@@ -1,25 +1,30 @@
-const express = require('express');
-const mongoose = require('mongoose');
-const cors = require('cors');
-require('dotenv').config();
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 
 // Connect MongoDB
-mongoose.connect(process.env.MONGO_URI, {
+mongoose
+  .connect(process.env.MONGO_URI, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
-})
-    .then(() => console.log("MongoDB connected"))
-    .catch(err => console.log(err));
+  })
+  .then(() => console.log("MongoDB connected"))
+  .catch((err) => console.log(err));
 
 // Routes
-const authRoutes = require('./routes/auth');
-const quizRoutes = require('./routes/quiz');
+const authRoutes = require("./routes/auth");
+const quizRoutes = require("./routes/quiz");
 
-app.use('/api/auth', authRoutes);
-app.use('/api/quiz', quizRoutes);
+app.use("/api/auth", authRoutes);
+app.use("/api/quiz", quizRoutes);
+app.get("/welcome", (req, res) => {
+    res.send("Welcome to the backend!");
+  });
+  
 
 app.listen(process.env.PORT, () => console.log("Server started on port 5000"));
